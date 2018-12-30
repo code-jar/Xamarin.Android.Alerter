c1149cd3bf6596ef4405932880efd80e0b346268
admin_dfzc <admin_dfzc>
2018-12-30 17:17:00 +0800

    xamarin android alerter

---
+++ AlerterDemo/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Com.Tapadoo.Alerter;
+
+namespace AlerterDemo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var btn = FindViewById<Button>(Resource.Id.showBtn);
+            btn.Click += (sender, args) =>
+            {
+                Alerter.Create(this)
+                .SetTitle("Alert Title")
+                .SetText("Alert text...")
+                .Show();
+
+
+                //System.Threading.Tasks.Task.Run(() =>
+                //{
+                //    System.Threading.Thread.Sleep(3000);
+                //    if (Alerter.IsShowing)
+                //    {
+                //        Alerter.Hide();
+                //    }
+                //});
+            };
+        }
+    }
+}
